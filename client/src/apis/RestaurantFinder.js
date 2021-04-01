76a251b36b2c95174d3bb134ea5323d00986a836
Sandeep Gupta <sandeepguptabc@gmail.com>
2021-04-01 18:54:27 +0530

    prod set

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+let baseUrl = process.env.NODE_ENV === "production" ? "" : "http://localhost:4800"
+
 export default axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL+"/restaurants"
+    baseURL: baseUrl +process.env.REACT_APP_API_BASE_URL+"/restaurants"
 })
